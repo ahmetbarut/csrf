a1f7f2bd4ac7dace64cd04a88a484391496882cc
Ahmet Barut <iletisim@ahmetbarut.net>
2021-06-13 16:26:19 +0300

    first commit

---
+++ src/Csrf.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace ahmtbarut\Csrf;
+
+class Csrf
+{
+    /**
+     * Tokeni tutar
+     * @var string
+     */
+    protected string $token;
+
+    public function __construct()
+    {
+        if (session_status() === PHP_SESSION_NONE)
+        {
+            session_status();
+        }
+        $this->generateToken();
+    }
+
+    /**
+     * Yeni bir token oluşturur
+     */
+    public function generateToken()
+    {
+        $this->token = $_SESSION["_token"] = md5(time() . rand(0,9999));
+    }
+
+    /**
+     * tokenin varlığını kontrol eder.
+     * @param array|object $post
+     * @return bool
+     */
+    public function tokenHas(array | object $post): bool
+    {
+        if (is_array($post))
+        {
+            $post = (object) $post;
+        }
+        if ($post)
+        {
+            if ($post->_token === $this->token)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    /**
+     * Oluşturulan Tokeni getirir.
+     * @return string
+     */
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+
+    /**
+     * Hata durumunda dönecek hata kodu ve mesajı
+     * @param int $code
+     * @param string $message
+     */
+    public function error($code = 419, $message = "Page Expired")
+    {
+        return header("HTTP/1.1 {$code} {$message}");
+    }
+}
